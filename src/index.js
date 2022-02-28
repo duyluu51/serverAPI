3447dc7299eb8b25df85cceaa06bfcba5e4b6d9f
duyluu51 <duyluu51@gmail.com>
2022-02-28 18:53:20 +0700

    better

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const cors = require('cors')
 const app = express()
-const port = 5000
+const port = (process.env.PORT || 5000)
 
 // Rounter
 const route = require('./routes');

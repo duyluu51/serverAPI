d3b9f4421ae6b3b19d7f23a8a1ce97fa2eab12e4
duyluu51 <duyluu51@gmail.com>
2022-02-28 22:44:45 +0700

    better 3

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect("mongodb+srv://duyluu51:Anhvaem321@cluster0.eqcte.mongodb.net/wed_sale_product_dev?retryWrites=true&w=majority")
         console.log('Mongodb connect sucessfully')
     } catch (error) {
         console.log('Mongodb connect failure')
